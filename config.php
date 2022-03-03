56a52209ad52e2cb78bef93cc8a2c6a7fef459e8
Bigosik <62602572+Bigosik@users.noreply.github.com>
2022-03-03 23:53:00 +0100

    Define "_APPROX"

--- config.php
+++ config.php
@@ -4,4 +4,5 @@ define('_SERVER_URL', 'http://'._SERVER_NAME);
 define('_APP_ROOT', '/php_01_widok_kontroler');
 define('_APP_URL', _SERVER_URL._APP_ROOT);
 define("_ROOT_PATH", dirname(__FILE__));
+define("_APPROX", 0.0000001);//wymagana dokładność pierwiastkowania
 ?>
