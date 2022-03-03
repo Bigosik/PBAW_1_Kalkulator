7312057ae4fcc75ad1b8c5fead7e8b2c8abcd0ae
Bigosik <62602572+Bigosik@users.noreply.github.com>
2022-03-03 23:50:21 +0100

    Create function.php Create functions: invert, power, root

---
+++ app/function.php
@@ -0,0 +1,31 @@
+<?php
+
+function invert($value, $exp_num, $exp_den) //odwraca liczbę; exp_num(exponent numerator)=licznik wykładnika, exp_den(exponent denominator)=mianownik wykładnika
+{
+  if ($exp_num*$exp_den < 0)
+    return 1/$value;
+  else
+    return $value;
+}
+
+function power($base, $exponent)  //funkcja potęgująca, base=podstawa, exponent=wykładnik
+{
+  if ($exponent == 0)
+    return 1;
+  if ($exponent == 1)
+    return $base;
+  else
+    return $base * power($base, $exponent - 1);
+}
+
+function root($base, $exponent) //funcja pierwiastkująca, root=pierwiastek
+{
+  $xn = $base;
+  do
+  {
+    $x = $xn;
+    $xn = 1/$exponent*(($exponent-1)*$x+($base/power($x,$exponent-1));
+  }
+  while (abs($x - $xn) < _APPROX);
+  return $xn;
+}
