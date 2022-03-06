7f73f3ea054e501ff254aa333f1335d118c9d826
Bigosik <62602572+Bigosik@users.noreply.github.com>
2022-03-06 23:30:30 +0100

    Change "_APP_ROOT..."

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('_SERVER_NAME', 'localhost');
 define('_SERVER_URL', 'http://'._SERVER_NAME);
-define('_APP_ROOT', '/php_01_widok_kontroler');
+define('_APP_ROOT', '/PBAW_1_Kalkulatorek');
 define('_APP_URL', _SERVER_URL._APP_ROOT);
 define("_ROOT_PATH", dirname(__FILE__));
 define("_APPROX", 0.0000001);//wymagana dokładność pierwiastkowania
